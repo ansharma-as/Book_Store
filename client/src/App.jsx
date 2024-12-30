fcb11ef6879e2aaa4b93dc39b839cbf263fb36d3
ansharma-as <anshsharma712.as@gmail.com>
2024-12-30 23:31:38 +0530

    adding changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,11 +7,11 @@ function App() {
 
   return (
     <>
-    <nav></nav>
+    <nav>Navbar</nav>
     <main className="min-h-screen max-w-screen-2xl mx-auto px-4 py-6">
      <Outlet />
      </main>
-     <footer></footer>
+     <footer>Footer</footer>
     </>
   )
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 
 import { RouterProvider } from 'react-router-dom'
+import router from './routers/router'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
