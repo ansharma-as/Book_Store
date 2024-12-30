8be37dc686142024f9e5bb3c8b708fe55749e2a0
ansharma-as <anshsharma712.as@gmail.com>
2024-12-30 21:21:35 +0530

    adding files#2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Outlet } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     
+    <nav></nav>
+    <main className="min-h-screen max-w-screen-2xl mx-auto px-4 py-6">
+     <Outlet />
+     </main>
+     <footer></footer>
     </>
   )
 }
